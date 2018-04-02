ae2f570cdb385768f5ad9c4d2a1f241d764421b0
Rostyk Chaikivskyi <r.chaikivskyi@gmail.com>
2018-04-02 17:51:47 +0300

    add init js

---
+++ js/init.js
@@ -0,0 +1,8 @@
+/*jslint browser: true, white: true, plusplus: true, regexp: true, indent: 4, maxerr: 50, es5: true */
+/*jshint multistr: true, latedef: nofunc */
+/*global jQuery, $, Swiper*/
+
+$(document).ready(function() {
+    'use strict';
+
+});
